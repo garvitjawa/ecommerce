8ba697f2dcf686cbbbda076d870927b445832164
garvitjawa <gaminggarvit@gmail.com>
2025-09-06 23:31:55 +0530

    fix: cors

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -4,24 +4,27 @@ import dotenv from 'dotenv';
 import authRoutes from './routes/auth.route';
 import userRoutes from './routes/user.route';
 import connectDb from './config/mongodb';
-(async (): Promise<void> => {
-    try {
-        await connectDb();
-        const app = express();
-        app.use(cors({
-          origin: 'http://localhost:5173',
-          credentials: true
-        }));
-        app.use(express.json());
-        dotenv.config();
-        app.use('/auth', authRoutes);
-        app.use('/user', userRoutes);
-        app.listen(process.env.PORT, () => {
-            console.log(`Server running on port ${process.env.PORT}`);
-        });
-    }
-    catch(err){
-        console.log("Failed to start server:", err);
-    }
 
+dotenv.config();
+
+(async (): Promise<void> => {
+  try {
+    await connectDb();
+    const app = express();
+    app.use(
+      cors({
+        origin: ['http://localhost:5173', process.env.FRONTEND_URL || ''],
+        credentials: true,
+      }),
+    );
+    app.use(express.json());
+    app.use('/auth', authRoutes);
+    app.use('/user', userRoutes);
+    const port = process.env.PORT || 3000;
+    app.listen(port, () => {
+      console.log(`Server running on port ${port}`);
+    });
+  } catch (err) {
+    console.log('Failed to start server:', err);
+  }
 })();
